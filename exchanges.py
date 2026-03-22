ff7d8e6878daff48ff055248854a39134cf0ee09
yarkoroman777 <yarkoroman86@gmail.com>
2026-03-22 06:52:14 +0200

    Update exchanges.py

--- exchanges.py
+++ exchanges.py
@@ -1,1 +1,14 @@
+import ccxt
+import config
 
+def create_binance_client():
+    """Создаёт и возвращает клиент Binance с API-ключами."""
+    client = ccxt.binance({
+        'apiKey': config.BINANCE_API_KEY,
+        'secret': config.BINANCE_SECRET,
+        'enableRateLimit': True,       # автоматическое соблюдение лимитов
+        'options': {
+            'defaultType': 'spot',
+        }
+    })
+    return client
